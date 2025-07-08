afa01559139b85768be38270e4300da58b45eff4
AlumnoSoloYolo <manuel.santamaria.gonzalez.al@iespoligonosur.org>
2025-07-08 22:27:53 +1400

    Update environments.ts
    variables hardcodeadas para producción. problemas con el build time.time

--- app-peliculas-frontend/app-frontend-cometafilms/src/environments/environments.ts
+++ app-peliculas-frontend/app-frontend-cometafilms/src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    tmdbToken: process.env['TMDB_TOKEN'] || '',
-    apiUrl: process.env['API_URL'] || '',
-    movieApiUrl: process.env['MOVIE_API_URL'] || ''
+    tmdbToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTc3MjUyNzIzYjdhMDhmNDdmNTcxNmVlNjcwOThkNSIsIm5iZiI6MTczODU4NDgwNy45Miwic3ViIjoiNjdhMGIyZTcwNGNiNmY0MWI5Y2I4Y2NjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.4_LnrBtw5KaMsNXw8uGjxBSsxFPzW-63-v9srnr5iZg',
+    apiUrl: 'https://cometa-films-despliegue-production.up.railway.app',
+    movieApiUrl: 'https://api.themoviedb.org/3'
 };
